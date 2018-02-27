834412a0597dab59ce503afc6384189661bc4997
Julian Chan <julianchan@mail.weber.edu>
2018-02-27 08:44:50 -0700

    initial Commit

---
+++ mid/julian_chan_exam1_task1.c
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  julian_chan_exam1_task1.c
+ *
+ *    Description:  First choice.
+ *
+ *        Version:  1.0
+ *        Created:  02/27/2018 08:42:03 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Julian Chan (), julianchan@weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+
+    return 0;
+}
+// Function Definitions
+
+
